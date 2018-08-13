d6fec2074bd54ba7675b436a206513abbd6f7e48
shuaibike <1228813169@qq.com>
2018-08-13 23:16:47 +0800

    disicizuoye

--- TCP/serversocket/serversocket.java
+++ TCP/serversocket/serversocket.java
@@ -6,29 +6,35 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class serversocket {
-    public static  void main(String args[]) throws Exception {
-        ServerSocket ss=new ServerSocket(3203);  //
-        Socket s=ss.accept();   //链接客户端
-        String ip=s.getInetAddress().getHostAddress();  //获取客户端IP地址
-        int port=s.getLocalPort();//获取客户端端口号
-        System.out.println(ip);//打印IP地址表示与客户端连接成功
-        BufferedReader bufrin =
-                new BufferedReader(new InputStreamReader(s.getInputStream()));  //建立读取缓冲区输入流
-        PrintWriter out=
-                new PrintWriter(new FileWriter("d:/Server.txt"),true);  //建立写入文件流
-        String line=null;
-        while((line=bufrin.readLine())!=null){
-            Date date = new Date();//获取当前时间
-            String dateStr = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(date);//将时间转化为yyyy-MM-dd hh:mm:ss格式
-            System.out.println(line);//打印客户端发送的信息表示接收成功
-            out.print("时间:"+dateStr+"     ");
-            out.print("ip:"+ip+"     ");
-            out.print("端口:"+port+"     ");
-            out.println(line);
+    public static  void main(String args[]){
+        try {
+            ServerSocket ss = new ServerSocket(3203);  //
+            Socket s = ss.accept();   //链接客户端
+            String ip = s.getInetAddress().getHostAddress();  //获取客户端IP地址
+            int port = s.getLocalPort();//获取客户端端口号
+            System.out.println(ip);//打印IP地址表示与客户端连接成功
+            BufferedReader bufrin =
+                    new BufferedReader(new InputStreamReader(s.getInputStream()));  //建立读取缓冲区输入流
+            PrintWriter out =
+                    new PrintWriter(new FileWriter("d:/Server.txt"), true);  //建立写入文件流
+            String line = null;
+            while ((line = bufrin.readLine()) != null) {
+                Date date = new Date();//获取当前时间
+                String dateStr = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(date);//将时间转化为yyyy-MM-dd hh:mm:ss格式
+                System.out.println(line);//打印客户端发送的信息表示接收成功
+                out.print("时间:" + dateStr + "     ");
+                out.print("ip:" + ip + "     ");
+                out.print("端口:" + port + "     ");
+                out.println(line);
+            }
+            out.close();
+            s.close();
+            ss.close();
+        }catch(Exception e){
+            System.out.println("发生异常"+e);
+            e.printStackTrace();
         }
-        out.close();
-        s.close();
-        ss.close();
+
     }
 
 }
--- TCP/socket/socket.java
+++ TCP/socket/socket.java
@@ -5,19 +5,24 @@ import java.net.*;
 
 public class socket {
 
-    public static void main(String args[]) throws IOException {
-        Socket s=new Socket("127.0.0.1",3203);    //定义IP地址和端口号
-        BufferedReader bufr=
-                new BufferedReader(new InputStreamReader(System.in));   //将键盘输入的信息写入缓冲区输入流
-        BufferedWriter bufw=
-                new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));  //将信息写入socket缓冲区输出流
-        String line=null;
-        while((line=bufr.readLine())!=null){
-            bufw.write(line);    //发送信息
-            bufw.newLine();  //换行操作，用于读取
-            bufw.flush();  //将缓冲区的内容刷新
+    public static void main(String args[]) {
+        try{
+            Socket s=new Socket("127.0.0.1",3203);    //定义IP地址和端口号
+            BufferedReader bufr=
+                    new BufferedReader(new InputStreamReader(System.in));   //将键盘输入的信息写入缓冲区输入流
+            BufferedWriter bufw=
+                    new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));  //将信息写入socket缓冲区输出流
+            String line=null;
+            while((line=bufr.readLine())!=null){
+                bufw.write(line);    //发送信息
+                bufw.newLine();  //换行操作，用于读取
+                bufw.flush();  //将缓冲区的内容刷新
+            }
+            bufr.close();
+            s.close();
+        }catch (Exception e){
+            System.out.println("发生异常"+e);
+            e.printStackTrace();
         }
-        bufr.close();
-        s.close();
     }
 }
